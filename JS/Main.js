b581c208eb3ea1d117b3a49f350e90d5879c3b09
Lucas <135282384+LukkaG@users.noreply.github.com>
2026-05-25 16:48:15 -0300

    agora e o ultimo
    ultimo

--- JS/Main.js
+++ JS/Main.js
@@ -22,6 +22,11 @@ const dbURI = process.env.MONGO_URI;
 
 console.log('Link encontrado pelo Node:', dbURI);
 
+app.get('/', (req, res) => {
+    // Ajuste o caminho conforme onde o seu arquivo Index.html realmente está
+    res.sendFile(path.resolve(__dirname, '../', 'Index.html')); 
+});
+
 app.post('/finalizar-compra', verificarToken, async (req, res) => {
     try {
         const { itens, total } = req.body;
